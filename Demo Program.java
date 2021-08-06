779ee8b2d3ff8f16b83ff72840d9e1aeab02fa05
TylerP1999 <tyler.petchiny@gmail.com>
2021-08-05 21:19:32 -0400

    first commit

---
+++ Demo Program.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class questionfour{
+	
+public static void main(String[] args) {
+	
+	
+	int[]array1 = {10, 5, 15, 20, 25};
+	
+	for(int i= array1.length-1; i>=0; i-- ) {
+	
+	System.out.print(" " + array1[i]);
+}
+}
+}
